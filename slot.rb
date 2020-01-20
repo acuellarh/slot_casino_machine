e217ceec6dbfcf0cde4b5f3aedd638b7b7757cfb
Alejandro <acuellarh@gmail.com>
2020-01-20 17:27:32 -0500

    Program ok -  with code for test lines 51 to 54

--- slot.rb
+++ slot.rb
@@ -37,7 +37,6 @@ class Board
     @simbol_list = simbol_list
     @simbol_list_random = []
     @simbol_list_random_array = []
-    #random_generator
   end  
   
   def random_generator
@@ -45,24 +44,16 @@ class Board
       value = @simbol_list.shuffle.first
       @simbol_list_random = @simbol_list_random.push(value) 
     end
-    # CODE FOR TEST
-    #  array_for_test = [["J"],["J"],["J"], ["Q"],["K"],["Cat"],["J"],["Q"],["Monkey"],["Bird"],["Bird"],["Bird"],["J"],["Q"],["A"]]
-    #  array_for_test = [["J"],["J"],["J"], ["Q"],["K"],["Cat"],["J"],["Q"],["Monkey"],["Bird"],["Bird"],["Bird"],["J"],["Q"],["A"]]
-      array_for_test = [["J"],["J"],["J"], ["Q"],["K"],["Cat"],["Cat"],["Cat"],["Cat"],["Bird"],["Bird"],["Bird"],["A"],["Bird"],["Bird"]]
-      @simbol_list_random_array = array_for_test.each_slice(5).to_a
-    # END OF CODE FOR TEST
-    
-    #print  @simbol_list_random 
+    #print  print  "board: #{@simbol_list_random}" 
     #puts
     #@simbol_list_random_array = @simbol_list_random.each_slice(5).to_a
-
-
+    # CODE FOR TEST FORCING THE ENTRY, just comment the 3 lines of above, and delete the character "#" from the following 4 lines   
+      print  "board: #{@simbol_list_random}" 
+      puts
+      array_for_test = [["J"],["J"],["J"], ["Q"],["K"],["Cat"],["Cat"],["Cat"],["Cat"],["Bird"],["Bird"],["Bird"],["Bird"],["Bird"],["Bird"]]
+      @simbol_list_random_array = array_for_test.each_slice(5).to_a
+    # END OF CODE FOR TEST
   end
-
-  # def print_board
-  #   print @simbol_list_random
-  #   puts  
-  # end
 
 end
 
@@ -71,89 +62,66 @@ class ReadLine
   def initialize (board_array, array_paylines)
     @board_array = board_array
     @array_paylines = array_paylines
-    @index_and_countmatch = Hash.new
-    @pay_out = 0
+    # hash to save 
+    @payline_and_countmatch = Hash.new
   end
   
-  def hit!
-  #  puts "inicio"
-  #  print @board_array.random_generator
-   puts 
-
-   @board_array.random_generator.each_with_index do |a,index|
-     count = 0
-     new_a = []
-     a.each_with_index do |i, index2|
-         if index2 == 0
-             new_a.push(i)
-         elsif new_a[index2 -1] == i
-             new_a.push(i)
-         else
-             new_a = new_a
-         end
-     end
-
-      print  "Matching with index:  #{index} and  #{new_a.length} times "
-      puts
-     if new_a.length > 2
+  def hit!   
+    # cicle to check the array that content the payline, each one is saved in one position into an array [[payline0],[payline1],[payline2]] 
+    @board_array.random_generator.each_with_index do |a,index|  
+      new_a = []
+      #Cicle to compare the characters
+      a.each_with_index do |i, index2|
+          if index2 == 0
+              new_a.push(i)
+          elsif new_a[index2 -1] == i
+              new_a.push(i)
+          else
+              new_a = new_a
+          end
+      end
       #print  "Matching with index:  #{index} and  #{new_a.length} times "
-      #puts     
+      #puts
 
-      @index_and_countmatch[@array_paylines[index]] = new_a.length
-      #@index_and_countmatch[index] = new_a.length
+      # Values are addedd to the hash if the matches greater than 2
+      if new_a.length > 2
+        @payline_and_countmatch[@array_paylines[index]] = new_a.length      
+      end     
+    end
 
-    end     
-   end
-
-   if @index_and_countmatch.length != 0  
-    print @index_and_countmatch
-    puts
-    bet_compute
-   else
-    print "No ganaste"  
-   end
+    # If found matches print the payline(s)
+    if @payline_and_countmatch.length != 0  
+      print "paylines: #{@payline_and_countmatch}"
+      puts
+      bet_compute
+    else
+      print "Sorry Not Won, execute again the script, ruby slot.rb" 
+    end
     
   end
 
   def bet_compute
     gain = 0
     bet = 100
-    @index_and_countmatch.each do |key, value| 
-      print "key: #{key}"
-      puts 
-      print "value: #{value}"
-      puts
-      
-      # if value == 3
-      #   # gain = gain + bet*0.2
-      #   puts "3"
-      #   puts 
-      # elsif value == 4
-      #   # gain = gain + bet*2  
-      #   puts "4"
-      #   puts 
-      # else
-      #   # gain = gain + bet*10
-      #   puts "nada"
-      #   puts
-
-      # end
+    @payline_and_countmatch.each do |payline, n_matches| 
+      if n_matches == 3
+        gain = gain + bet*0.2              
+      elsif n_matches == 4
+        gain = gain + bet*2               
+      else
+        gain = gain + bet*10       
+      end
     end
-    # print "bet_amount: #{bet}"
-    # puts
-    # print "total_win:  #{gain}"
+    print "bet_amount: #{bet}"
+    puts
+    puts "total_win:  #{gain}"
   end
-  
-  
+
 end
 
 simbol_list = [["10"],["J"],["Q"], ["A"],["K"],["Cat"],["Dog"],["Monkey"],["Bird"]]
 array_paylines = ["0 3 6 9 12","1 4 7 10 13","2 5 8 11 14"]
-# play = Board.new(simbol_list)
-# board_array = play.random_generator
 board_array = Board.new(simbol_list)
-
-#play.print_board
 read_line = ReadLine.new(board_array, array_paylines)
 read_line.hit!
 
